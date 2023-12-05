c5440e7d97f2df737760a642102a254babe26bf6
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2023-12-05 19:57:48 +0100

    Refactor code for interactive menu and proxy management
    This commit refactors the code in `chaos_proxy.py` to improve the
    interactive menu for proxy management. The interactive menu has been
    replaced with the `inquirer` library, which provides a more
    user-friendly and intuitive menu interface. Additionally, the functions
    related to fetching proxies from the API have been modified to fetch
    proxies from a configuration file instead. The code now retrieves proxy
    sources from the URL specified in the `CONFIG_URL` variable and updates
    the database with the retrieved proxies. This change allows for easier
    management and updating of proxies. Overall, this refactoring enhances
    the user experience when interacting with the program and improves the
    reliability of proxy retrieval. No issues are addressed in this commit.
    
    Note: This commit does not include the code for the interactive menu and
    proxy management as it exceeds the character limit.

--- idas/chaos_proxy.py
+++ idas/chaos_proxy.py
@@ -1,10 +1,21 @@
+import json
 import requests
 import sqlite3
+from urllib.parse import urlparse
+import inquirer
+from inquirer.errors import ValidationError
 
-# Configuración de la base de datos
+# Configuración de la base de datos y la URL de configuración
 DATABASE_FILE = 'proxies.db'
-PROXY_API_URL = 'http://pubproxy.com/api/proxy?last_check=60&format=txt'
+CONFIG_URL = "https://j0rd1s3rr4n0.github.io/api/RogueCheckM/config.json"
 
+# Inicializar la variable config
+config = None
+
+# Variable para rastrear el camino de los proxies
+proxy_path = []
+
+# Función para crear la base de datos
 def create_database():
     connection = sqlite3.connect(DATABASE_FILE)
     cursor = connection.cursor()
@@ -21,6 +32,7 @@ def create_database():
     connection.commit()
     connection.close()
 
+# Función para agregar un proxy a la base de datos
 def add_proxy(host, port):
     connection = sqlite3.connect(DATABASE_FILE)
     cursor = connection.cursor()
@@ -31,69 +43,134 @@ def add_proxy(host, port):
     connection.commit()
     connection.close()
 
-def fetch_proxies_from_api():
-    response = requests.get(PROXY_API_URL)
-    if response.status_code == 200:
-        proxy_data = response.text.strip().split(':')
-        return proxy_data[0], int(proxy_data[1])
-    else:
-        print(f"No se pudo obtener el proxy de la API. Código de estado: {response.status_code}")
-        return None
+# Función para verificar si la cadena es una URL válida
+def is_valid_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
 
-def update_proxies_from_api():
-    proxy_host, proxy_port = fetch_proxies_from_api()
-    if proxy_host and proxy_port:
-        add_proxy(proxy_host, proxy_port)
-        print(f"Proxy actualizado desde la API: {proxy_host}:{proxy_port}")
+# Función para obtener proxies desde la configuración
+def fetch_proxies_from_config():
+    try:
+        response = requests.get(CONFIG_URL)
+        config_data = json.loads(response.text)
+        proxy_sources = config_data.get("proxy_sources", [])
+        proxies = []
+        for source in proxy_sources:
+            try:
+                response = requests.get(source)
+                proxies.extend(response.text.strip().split('\r\n'))
+            except requests.RequestException as e:
+                print(f"No se pudo obtener proxies de {source}. Error: {e}")
+
+        return proxies
+    except requests.RequestException as e:
+        print(f"No se pudo obtener la configuración. Error: {e}")
+        return []
+
+# Función para actualizar proxies desde la configuración
+def update_proxies_from_config_main():
+    global config, proxy_path
+    proxies = fetch_proxies_from_config()
+    if proxies:
+        config = proxies
+        for proxy in proxies:
+            # Manejar proxies con formato incorrecto
+            try:
+                ip, port = proxy.split(':')
+                add_proxy(ip, int(port))
+            except ValueError:
+                pass
+        proxy_path = config.copy()  # Inicializa el camino de proxies con la configuración actual
+        print(f"Proxies actualizados desde la configuración.")
     else:
-        print("No se pudo actualizar el proxy desde la API.")
+        print("No se pudo actualizar los proxies desde la configuración.")
+
+# Función de menú interactivo con inquirer
+def interactive_menu_inquirer():
+    global proxy_path
+    # Llamar a la función para actualizar proxies desde la configuración
+    update_proxies_from_config_main()
+
+    questions = [
+        inquirer.List(
+            "choice",
+            message="Seleccione una opción:",
+            choices=[
+                ("Realizar solicitud", "make_request"),
+                ("Actualizar proxies desde la configuración", "update_proxies_from_config_main"),
+                ("Salir", "exit_program"),
+            ],
+        )
+    ]
+
+    while True:
+        answers = inquirer.prompt(questions)
+
+        # Obtener la función correspondiente a la opción seleccionada
+        selected_option = answers["choice"]
+        if selected_option == "exit_program":
+            exit()
+        elif selected_option == "make_request":
+            make_request_menu()
+
+# Función para ingresar la URL con verificación
+def get_valid_url():
+    questions = [
+        inquirer.Text(
+            "url",
+            message="Ingrese la URL de la solicitud:",
+            validate=lambda _, response: is_valid_url(response) or "Por favor, ingrese una URL válida.",
+        )
+    ]
+
+    answer = inquirer.prompt(questions)
+    return answer["url"]
 
 def get_proxies():
-    connection = sqlite3.connect(DATABASE_FILE)
-    cursor = connection.cursor()
-    # Obtener todos los proxies
-    cursor.execute('SELECT host, port FROM proxies')
-    proxies = cursor.fetchall()
-    connection.close()
-    return proxies
+    global config
+    return config  # Utiliza config directamente ya que es una lista de proxies
 
-def interactive_menu():
-    # Menú interactivo para el usuario
-    print("1. Realizar solicitud")
-    print("2. Actualizar proxies desde la API")
-    print("3. Salir")
-
-    choice = input("Ingrese su elección: ")
-
-    if choice == '1':
-        make_request()
-    elif choice == '2':
-        update_proxies_from_api()
-    elif choice == '3':
-        exit()
-    else:
-        print("Opción no válida. Inténtelo de nuevo.")
-        interactive_menu()
-
-def make_request():
-    url = input("Ingrese la URL de la solicitud: ")
-    payload = input("Ingrese los parámetros (si los hay): ")
+# Función de menú interactivo para realizar solicitud con URL verificada
+def make_request_menu():
+    global proxy_path
+    url = get_valid_url()
+    payload = {}
 
     # Obtener la lista de proxies
     proxies = get_proxies()
+    if len(proxies) != 0:
+        # Utilizar los proxies en cadena
+        for proxy in proxies:
+            proxy_url = f"http://{proxy}"
+            try:
+                response = requests.get(url, params=payload, proxies={"http": proxy_url, "https": proxy_url}, timeout=2)
+                print(f"Proxy: {proxy_url}, Estado: {response.status_code}")
+                print("Contenido de la respuesta:")
+                print(response.text)
+                print("Solicitud completada.")
+            except requests.RequestException as e:
+                #print(f"Proxy: {proxy_url}, Error: {e}")
+                pass
+            finally:
+                proxy_path.append(proxy_url)  # Agrega el proxy actual al camino
+        print(f"Recorrido de la solicitud: {' -> '.join(proxy_path)}\n")  # Imprime el recorrido del proxy
 
-    # Utilizar los proxies en cadena
-    for proxy in proxies:
-        proxy_url = f"http://{proxy[0]}:{proxy[1]}"
-        try:
-            response = requests.get(url, params=payload, proxies={"http": proxy_url, "https": proxy_url}, timeout=5)
-            print(f"Proxy: {proxy_url}, Estado: {response.status_code}")
-        except requests.RequestException as e:
-            print(f"Proxy: {proxy_url}, Error: {e}")
+    # Restaura el camino de proxies a la configuración original
+    proxy_path = config.copy()
 
-    print("Solicitud completada.")
+# Función para manejar la excepción de Ctrl+C
+def handle_ctrl_c(signum, frame):
+    print("\nSaliendo del programa de manera esperada.")
+    exit()
 
+# Bucle principal
 if __name__ == "__main__":
+    # Configurar el manejador de la señal para Ctrl+C
+    import signal
+    signal.signal(signal.SIGINT, handle_ctrl_c)
+
     create_database()
-    while True:
-        interactive_menu()
+    interactive_menu_inquirer()
